c9b85b357f8a5759beafbb6dd938000147283d15
cookie-s <kcz@kcz.sh>
2020-06-01 22:31:38 +0900

    init

---
+++ init.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long int;
+using lc = complex<double>;
+
+int main(void) {
+    constexpr ll MOD = 1e9 + 7;
+    constexpr double PI = acos(-1);
+    cout << fixed << setprecision(32);
+    cin.tie(0); ios::sync_with_stdio(false);
+
+
+}
