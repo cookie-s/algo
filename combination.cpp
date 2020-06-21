7cfd841def9db345992826a25ccb7d4e30e130b9
cookie-s <kcz@kcz.sh>
2020-06-21 23:49:41 +0900

    comb

--- combination.cpp
+++ combination.cpp
@@ -1,30 +1,28 @@
 using ll = long long;
 
-template<ll MOD = 1'000'000'007, ll MAX = 1'000'000>
-struct Mod {
+{
+    constexpr ll MAX = 2e6;
+
     vector<ll> fact, ifact;
-    Mod() {
-        fact.assign(MAX+1, 1);
-        for(ll i=1; i<=MAX; i++)
-            (fact[i] = i * fact[i-1]) %= MOD;
+    fact.assign(MAX+1, 1);
+    for(ll i=1; i<=MAX; i++)
+        (fact[i] = i * fact[i-1]) %= MOD;
 
-        ifact.assign(MAX+1, 1);
-        {
-            ll a = fact[MAX], b = MOD-2;
-            while(b) {
-                if(b&1) (ifact[MAX] *= a) %= MOD;
-                (a *= a) %= MOD;
-                b >>= 1;
-            }
+    ifact.assign(MAX+1, 1);
+    {
+        ll a = fact[MAX], b = MOD-2;
+        while(b) {
+            if(b&1) (ifact[MAX] *= a) %= MOD;
+            (a *= a) %= MOD;
+            b >>= 1;
         }
-        for(ll i=MAX-1; i>=0; i--)
-            (ifact[i] = ifact[i+1] * (i+1)) %= MOD;
     }
+    for(ll i=MAX-1; i>=0; i--)
+        (ifact[i] = ifact[i+1] * (i+1)) %= MOD;
 
-    ll comb(ll a, ll b) {
+    auto comb = [&](ll a, ll b) {
         if(a < b) return 0;
         return ((fact[a] * ifact[b]) % MOD * ifact[a-b]) % MOD;
-    }
-
+    };
 }
 
