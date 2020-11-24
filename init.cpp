a32682e6f1f5a81bc4fa612c678cfaa54dd60035
cookie-s <kcz@kcz.sh>
2020-11-24 21:15:53 +0900

    update init

--- init.cpp
+++ init.cpp
@@ -1,16 +1,27 @@
+// ＵＴＦ−８
 // Mac: g++ -Wall -Wl,-stack_size -Wl,100000000 a.cpp -std=c++2a -O3
 
 #include<bits/stdc++.h>
+/* #include<atcoder/all> */
+/* using namespace atcoder; */
 
 using namespace std;
 using ll = long long int;
-using lc = complex<double>;
+using lc = complex<ll>;
+
+template<class T>bool chmax(T &a, const T &b) { return (a<b ? (a=b,1) : 0); }
+template<class T>bool chmin(T &a, const T &b) { return (a>b ? (a=b,1) : 0); }
 
 int main(void) {
-    constexpr ll MOD = 1e9 + 7;
+    constexpr ll MOD = 1 ? 1e9+7 : 998244353;
     constexpr double PI = acos(-1);
     cout << fixed << setprecision(32);
     cin.tie(0); ios::sync_with_stdio(false);
 
+    if(1) {
+        // START
 
+    }
+
+    return 0;
 }
